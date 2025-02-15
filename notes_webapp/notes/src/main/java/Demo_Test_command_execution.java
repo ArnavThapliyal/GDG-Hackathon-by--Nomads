ede3b3c436ab6549f3fa9aef22090c6f129afbcc
Arnav Thapliyal <52890987+ArnavThapliyal@users.noreply.github.com>
2025-02-15 17:51:49 +0530

    Add files via upload

---
+++ notes_webapp/notes/src/main/java/Demo_Test_command_execution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Demo_Test_command_execution
+{
+    public static void main(String[] args) {
+        System.out.println("Jam");
+        Scanner obj = new Scanner(System.in);
+        int Var_1;
+        Var_1 = obj.nextInt();
+        System.out.println(Var_1);
+    }
+}
---
+++ notes_webapp/notes/src/test/java/com/nomads/notes/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomads.notes.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
